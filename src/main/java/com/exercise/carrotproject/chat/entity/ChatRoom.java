6c271b62516a9c1ee67f8260ae1b9af4753e2274
HoYo <skaghdyd@gmail.com>
2023-03-20 15:01:33 +0900

    호용 commit
    chat 관련 entity 추가
    loc enum과 converter 추가

---
+++ src/main/java/com/exercise/carrotproject/chat/entity/ChatRoom.java
@@ -0,0 +1,34 @@
+package com.exercise.carrotproject.chat.entity;
+
+import com.exercise.carrotproject.member.Member;
+import com.exercise.carrotproject.post.Post;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "chat_room")
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long roomId;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Member seller;
+
+    @ManyToOne
+    @JoinColumn(name = "buyer_id")
+    private Member buyer;
+
+    @OneToMany(mappedBy = "room")
+    private List<Chat> chatList;
+
+    @OneToMany(mappedBy = "room")
+    private List<ChatImg> chatImgList;
+}
--- src/main/java/com/exercise/carrotproject/enumlist/Loc.java
+++ src/main/java/com/exercise/carrotproject/enumlist/Loc.java
@@ -1,5 +1,30 @@
 package com.exercise.carrotproject.enumlist;
 
-public enum Loc {
+import lombok.Getter;
 
+import java.util.Arrays;
+
+@Getter
+public enum Loc {
+    GANGDONG("1","강동"),
+    GANGSEO("2","강서"),
+    GANGNAM("3","강남"),
+    GANGBUK("4","강북");
+
+    //필드
+    private String locCode;
+    private String locName;
+
+    //생성자
+    Loc(String locCode, String locName) {
+        this.locCode = locCode;
+        this.locName = locName;
+    }
+
+    public static Loc getLocNameByCode(String dbData) {
+        return Arrays.stream(Loc.values())
+                .filter(v -> v.getLocCode().equals(dbData))
+                .findAny()
+                .orElseThrow();
+    }
 }
