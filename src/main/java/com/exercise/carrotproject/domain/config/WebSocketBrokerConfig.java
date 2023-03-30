3c20b8bbe62d435560160ddab3d716013460d21d
HoYo <skaghdyd@gmail.com>
2023-03-30 14:07:27 +0900

    파일구조 업데이트
    파일구조 업데이트

--- src/main/java/com/exercise/carrotproject/domain/config/WebSocketBrokerConfig.java
+++ src/main/java/com/exercise/carrotproject/domain/config/WebSocketBrokerConfig.java
@@ -1,11 +1,11 @@
-package com.exercise.carrotproject.domain.chat.websocket;
+package com.exercise.carrotproject.domain.config;
 
+import com.exercise.carrotproject.domain.config.interceptor.MyHttpSessionHandshakeInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
