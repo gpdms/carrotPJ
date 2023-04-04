43d75b12783481a81d7c94dcf794d0c5a8e1f647
HoYo <skaghdyd@gmail.com>
2023-04-04 15:49:11 +0900

    채팅방 기능 업데이트
    채팅방 기능 업데이트

--- src/main/java/com/exercise/carrotproject/domain/config/WebConfig.java
+++ src/main/java/com/exercise/carrotproject/domain/config/WebConfig.java
@@ -2,11 +2,18 @@ package com.exercise.carrotproject.domain.config;
 
 import com.exercise.carrotproject.web.argumentresolver.LoginMemberArgumentResolver;
 import com.exercise.carrotproject.domain.config.interceptor.LoginCheckInterceptor;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -29,6 +36,18 @@ public class WebConfig implements WebMvcConfigurer {
 //                .excludePathPatterns("/members/signup",
 //                        "/members/css/**","/members/js/**","/members/assets/**",
 //                        "/members/error");
+    }
+
+    @Bean
+    public ServletContextInitializer clearJsession() {
+        return new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
+                SessionCookieConfig sessionCookieConfig=servletContext.getSessionCookieConfig();
+                sessionCookieConfig.setHttpOnly(true);
+            }
+        };
     }
 
 }
