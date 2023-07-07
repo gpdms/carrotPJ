e8c818f62c7641b72955cedeab03d37cadf9c5f7
kitcat09 <jk65333@gmail.com>
2023-07-07 16:14:27 +0900

    Refactor : MemberCustomRepository 삭제
    MemberCustomRepository안의 메서드들이 불필요하여 삭제
    1. Block관련 메서드들을 BlockRepository로 이동
    2. 임시 비밀번호로 업데이트 하는 로직은 QueryDsl대신 더티체킹으로 처리

--- src/main/java/com/exercise/carrotproject/domain/member/service/BlockServiceImpl.java
+++ src/main/java/com/exercise/carrotproject/domain/member/service/BlockServiceImpl.java
@@ -1,33 +1,16 @@
 package com.exercise.carrotproject.domain.member.service;
 
-import com.exercise.carrotproject.domain.enumList.Role;
-import com.exercise.carrotproject.domain.member.MemberEntityDtoMapper;
-import com.exercise.carrotproject.domain.member.dto.MemberDto;
 import com.exercise.carrotproject.domain.member.entity.Block;
 import com.exercise.carrotproject.domain.member.entity.Member;
 import com.exercise.carrotproject.domain.member.repository.BlockRepository;
-import com.exercise.carrotproject.domain.member.repository.MemberRepository;
-import com.exercise.carrotproject.domain.member.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import javax.mail.MessagingException;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.UUID;
 
 @Slf4j
 @Service
