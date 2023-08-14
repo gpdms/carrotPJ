e17662f0eb25f5290a6c4359be2eab9b28708ef5
kitcat09 <jk65333@gmail.com>
2023-08-15 00:10:52 +0900

    Fix: 소셜가입 validation관련 오류
     1. 역직렬화시 오류 발생
    역직렬화후 폼 객체가 immutable하기를 원해 필드에 final을 붙이고 setter를 없앴다. Ja기본 생성자 + setter가 있어야 맵핑하는데 없어서 실패
    -> @JsonCreator붙여 해결
    2. 멤버 Id를 6~12자 사이로 변경
    -> 소셜 로그인 아이디 생성하는 유틸에서 12자까지 substring하도록 수정

--- src/main/java/com/exercise/carrotproject/web/member/form/JoinSocialForm.java
+++ src/main/java/com/exercise/carrotproject/web/member/form/JoinSocialForm.java
@@ -1,23 +1,32 @@
 package com.exercise.carrotproject.web.member.form;
 
 import com.exercise.carrotproject.domain.enumList.Loc;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.validation.constraints.*;
 
 @Getter
 @ToString
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class JoinSocialForm {
     @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,15}$",
             message = "닉네임은 2~15자의 영문, 한글, 숫자만 사용가능합니다.")
     private final String nickname;
-
     @NotEmpty
     private final String email;
-
     @NotNull
     private final Loc loc;
-
     private final String profImgUrl;
+
+    @JsonCreator
+    protected JoinSocialForm(@JsonProperty("nickname") String nickname,
+                             @JsonProperty("email") String email,
+                             @JsonProperty("loc") Loc loc,
+                             @JsonProperty("profileImgUrl") String profileImgUrl) {
+        this.nickname = nickname;
+        this.email = email;
+        this.loc = loc;
+        this.profImgUrl = profileImgUrl;
+    }
 }
